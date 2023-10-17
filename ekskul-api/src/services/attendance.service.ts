23a393f120aa3f15d5bf236209d62a73a9f6e00f
yase08 <ayasbogor@gmail.com>
2023-10-17 10:24:09 +0700

    update attendance and schema

--- ekskul-api/src/services/attendance.service.ts
+++ ekskul-api/src/services/attendance.service.ts
@@ -14,28 +14,53 @@ export class AttendanceService {
       const accessToken: string = (req.headers.authorization as string).split(
         "Bearer "
       )[1];
-      const decodedToken: string | JwtPayload = await verifyToken(accessToken);
+      const decodedToken: any = await verifyToken(accessToken);
 
-      const ekskul = await db.ekskul.findOne({
-        where: { id: decodedToken["ekskul"] },
-      });
+      const ekskulIds = decodedToken.ekskul;
+      const selectedEkskulId = Number(req.query.ekskul_id);
 
-      const attendanceData = req.body;
+      if (ekskulIds.includes(selectedEkskulId)) {
+        const createAttendancePromises = [];
 
-      const createAttendancePromises = attendanceData.map(
-        async (attendance: any) => {
-          await db.attendance.create({ ...attendance, ekskul_id: ekskul.id });
+        for (const attendance of req.body) {
+          // Memeriksa apakah student_id terkait dengan ekskul_id tertentu
+          const studentId = attendance.student_id;
+
+          const studentOnEkskul = await db.studentOnEkskul.findOne({
+            where: { student_id: studentId, ekskul_id: selectedEkskulId },
+          });
+
+          if (studentOnEkskul) {
+            // Jika terkait, buat data kehadiran
+            const createAttendance = db.attendance.create({
+              ...attendance,
+              ekskul_id: selectedEkskulId,
+            });
+            createAttendancePromises.push(createAttendance);
+          } else {
+            // Jika tidak terkait, lewati data kehadiran ini dan beri respons error
+            throw apiResponse(
+              status.FORBIDDEN,
+              `Student with ID ${studentId} is not associated with the selected ekskul`
+            );
+          }
         }
-      );
 
-      const createAttendances = await Promise.all(createAttendancePromises);
+        const createdAttendances = await Promise.all(createAttendancePromises);
 
-      if (!createAttendances)
-        throw apiResponse(status.FORBIDDEN, "Create new attendances failed");
+        if (!createdAttendances.length) {
+          throw apiResponse(status.FORBIDDEN, "Create new attendances failed");
+        }
 
-      return Promise.resolve(
-        apiResponse(status.OK, "Create new attendance success")
-      );
+        return Promise.resolve(
+          apiResponse(status.OK, "Create new attendance success")
+        );
+      } else {
+        throw apiResponse(
+          status.NOT_FOUND,
+          "Ekskul does not exist for the given id"
+        );
+      }
     } catch (error: any) {
       return Promise.reject(
         apiResponse(
@@ -52,91 +77,89 @@ export class AttendanceService {
       const accessToken: string = (req.headers.authorization as string).split(
         "Bearer "
       )[1];
-      const decodedToken: string | JwtPayload = await verifyToken(accessToken);
+      const decodedToken: any = await verifyToken(accessToken);
 
-      const ekskul = await db.ekskul.findOne({
-        where: { id: decodedToken["ekskul"] },
-      });
+      const ekskulIds = decodedToken.ekskul;
+      const selectedEkskulId = req.query.ekskul_id;
 
       const sort: string =
         typeof req.query.sort === "string" ? req.query.sort : "";
-      const filter: string =
-        typeof req.query.filter === "string" ? req.query.filter : "";
       const page: any = req.query.page;
 
-      const paramQuerySQL: any = {
-        where: { ekskul_id: ekskul.id },
-      };
-      let limit: number;
-      let offset: number;
-
-      if (filter) {
-        paramQuerySQL.where = {
-          category: filter,
+      if (ekskulIds.includes(Number(selectedEkskulId))) {
+        const paramQuerySQL: any = {
+          where: { ekskul_id: selectedEkskulId },
         };
-      }
+        let limit: number;
+        let offset: number;
 
-      if (sort) {
-        const sortOrder = sort.startsWith("-") ? "DESC" : "ASC";
-        const fieldName = sort.replace(/^-/, "");
-        paramQuerySQL.order = [[fieldName, sortOrder]];
-      }
+        if (sort) {
+          const sortOrder = sort.startsWith("-") ? "DESC" : "ASC";
+          const fieldName = sort.replace(/^-/, "");
+          paramQuerySQL.order = [[fieldName, sortOrder]];
+        }
 
-      if (page && page.size && page.number) {
-        limit = parseInt(page.size, 10);
-        offset = (parseInt(page.number, 10) - 1) * limit;
-        paramQuerySQL.limit = limit;
-        paramQuerySQL.offset = offset;
-      } else {
-        limit = 10;
-        offset = 0;
-        paramQuerySQL.limit = limit;
-        paramQuerySQL.offset = offset;
-      }
+        if (page && page.size && page.number) {
+          limit = parseInt(page.size, 10);
+          offset = (parseInt(page.number, 10) - 1) * limit;
+          paramQuerySQL.limit = limit;
+          paramQuerySQL.offset = offset;
+        } else {
+          limit = 10;
+          offset = 0;
+          paramQuerySQL.limit = limit;
+          paramQuerySQL.offset = offset;
+        }
 
-      paramQuerySQL.include = [
-        {
-          model: db.ekskul,
-          attributes: ["name"],
-          as: "ekskul",
-        },
-        {
-          model: db.student,
-          attributes: ["name"],
-          as: "student",
-        },
-      ];
+        paramQuerySQL.include = [
+          {
+            model: db.ekskul,
+            attributes: ["name"],
+            as: "ekskul",
+          },
+          {
+            model: db.student,
+            attributes: ["name"],
+            as: "student",
+          },
+        ];
 
-      const attendances = await db.attendance.findAll(paramQuerySQL);
+        const attendances = await db.attendance.findAll(paramQuerySQL);
 
-      if (!attendances || attendances.length === 0) {
+        if (!attendances || attendances.length === 0) {
+          return Promise.resolve(
+            apiResponse(
+              status.NOT_FOUND,
+              "No attendances found with the specified filter"
+            )
+          );
+        }
+
+        const modifiedAttendances = attendances.map((attendance) => {
+          return {
+            id: attendance.id,
+            ekskul_id: attendance.ekskul.name,
+            student_id: attendance.student.name,
+            category: attendance.category,
+            date: attendance.date,
+            createdAt: attendance.createdAt,
+            updatedAt: attendance.updatedAt,
+          };
+        });
+
         return Promise.resolve(
           apiResponse(
-            status.NOT_FOUND,
-            "No attendances found with the specified filter"
+            status.OK,
+            "Fetched all attendances success",
+            modifiedAttendances
           )
         );
+      } else {
+        throw apiResponse(
+          status.NOT_FOUND,
+          "Ekskul do not exist for the given id"
+        );
       }
-
-      const modifiedAttendances = attendances.map((attendance) => {
-        return {
-          id: attendance.id,
-          ekskul_id: attendance.ekskul.name,
-          student_id: attendance.student.name,
-          category: attendance.category,
-          date: attendance.date,
-          createdAt: attendance.createdAt,
-          updatedAt: attendance.updatedAt,
-        };
-      });
-
-      return Promise.resolve(
-        apiResponse(
-          status.OK,
-          "Fetched all attendances success",
-          modifiedAttendances
-        )
-      );
     } catch (error: any) {
       return Promise.reject(
         apiResponse(
